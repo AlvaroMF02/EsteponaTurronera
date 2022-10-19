fe8a5824bc8632481ac3d4ff8584f82f0f0e440a
alvaro <amarflo1406@g.educaand.es>
2022-10-19 17:58:44 +0200

    uwu

--- src/main/java/dulces/TuEresMiDulce.java
+++ src/main/java/dulces/TuEresMiDulce.java
@@ -9,6 +9,8 @@ import javax.swing.JOptionPane;
 public class TuEresMiDulce {
 
     public static void main(String[] args) {
+        
+        //VARIABLES
         double manoObra;
         double materiaPrima;
         String pedido = "";
@@ -26,47 +28,46 @@ public class TuEresMiDulce {
 
         pedido = (JOptionPane.showInputDialog(menu));
 
-        //COSTE MANO DE OBRA
-        switch (pedido) {
-            case "M1","P1":
-                manoObra = 0.15;
-                break;
-
-            case "T1","T2","M2":
-                manoObra = 0.22;
-                break;
-
-            default:
-                System.out.println("Has introducido un codigo erroneo");
-        }
-
         //SACAR MATERIA PRIMA PARA CALCULAR EL COSTE DE PRODUCCION
         //(coste producción = materia prima + mano de obra)
         materiaPrima = Double.parseDouble(JOptionPane.showInputDialog
         ("¿Cuál será el coste de la materia prima?"));
-        
-        if (materiaPrima>0.1 && materiaPrima<=1 ) {
-            
-        }else{
+
+        //AQUI EMPIEZA TODO EL PROGRAMA
+        if (materiaPrima > 0.1 && materiaPrima <= 1) {
+
+            //COSTE MANO DE OBRA
+            switch (pedido) {
+                case "M1","P1":
+                    manoObra = 0.15;
+                    break;
+
+                case "T1","T2","M2":
+                    manoObra = 0.22;
+                    break;
+
+                default:
+                    System.out.println("Has introducido un codigo erroneo");
+            }
+
+            //VENTA UNITARIO
+            switch (pedido) {
+                case "M1","P2","P1":
+
+                    //Precio de venta unitario = coste de producción + 50 % del coste de producción
+                    break;
+
+                case "T1","T2","M2":
+
+                    //Precio de venta unitario = coste de producción + 65 % del coste de producción
+                    break;
+
+                default:
+                    System.out.println("Has introducido un codigo erroneo");
+            }
+
+        } else {
             JOptionPane.showInputDialog("LA MATERIA DEBE ESTAR ENTRE 0.1 Y 1");
-        }
-        
-        
-        
-        //VENTA UNITARIO
-        switch (pedido) {
-            case "M1","P2","P1":
-
-                //Precio de venta unitario = coste de producción + 50 % del coste de producción
-                break;
-
-            case "T1","T2","M2":
-
-                //Precio de venta unitario = coste de producción + 65 % del coste de producción
-                break;
-
-            default:
-                System.out.println("Has introducido un codigo erroneo");
         }
 
     }
